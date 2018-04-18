d8de00bc051da35514472f5a90fcd25d4f149e8a
Nikolai Magnussen <nikolai@magnussen.tf>
2018-04-18 11:06:46 +0200

    Rustfmt and increment number of segments when adding a new one

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 extern crate nix;
-extern crate reqwest;
 extern crate rand;
+extern crate reqwest;
 
 #[macro_use]
 extern crate serde_derive;
@@ -8,9 +8,9 @@ extern crate serde_derive;
 extern crate serde;
 extern crate serde_json;
 
-use nix::unistd::{execv, setsid, fork, gethostname, ForkResult};
+use nix::unistd::{execv, fork, gethostname, setsid, ForkResult};
 
-use std::io::{self, BufReader, BufRead, Read};
+use std::io::{self, BufRead, BufReader, Read};
 use std::fs::File;
 use std::net::{TcpListener, TcpStream};
 use std::collections::hash_map::DefaultHasher;
@@ -18,7 +18,7 @@ use std::collections::HashMap;
 use std::ffi::CString;
 use std::vec::Vec;
 use std::hash::Hasher;
-use std::time::{Instant, Duration};
+use std::time::{Duration, Instant};
 use std::env;
 
 #[derive(Deserialize, Serialize, Debug, Copy, Clone, PartialEq)]
@@ -26,7 +26,7 @@ enum TreeState {
     Child,
     Parent,
     Sibling,
-    This
+    This,
 }
 
 #[derive(Deserialize, Serialize, Debug)]
@@ -39,7 +39,7 @@ enum Message {
 #[derive(Deserialize, Serialize, Debug, PartialEq)]
 struct WormSegment {
     relationship: TreeState,
-    hostname: String
+    hostname: String,
 }
 
 #[derive(Deserialize, Serialize, Debug)]
@@ -47,10 +47,10 @@ struct Worm {
     initial_hostname: String,
     current_hostname: String, // Modify after sending
     max_num_segments: usize,
-    cur_num_segments: usize, // Modify before sending
+    cur_num_segments: usize,                   // Modify before sending
     observation_data: HashMap<String, String>, // Modify with gossiping and after getting data
     current_segments: Vec<WormSegment>, // Modify before sending and after sending (change state)
-    hosts_to_ovserve: Vec<String>
+    hosts_to_ovserve: Vec<String>,
 }
 
 impl WormSegment {
@@ -58,27 +58,23 @@ impl WormSegment {
     pub fn new(rel: TreeState, host: &str) -> WormSegment {
         WormSegment {
             relationship: rel,
-            hostname: String::from(host)
+            hostname: String::from(host),
         }
     }
 
     /// Convert WormSegment such that we can send it to another segment
     pub fn send_to(&self, target: &WormSegment) -> WormSegment {
         let new_rel = match target.relationship {
-            TreeState::Child => {
-                match self.relationship {
-                    TreeState::Child => TreeState::Sibling,
-                    TreeState::This => TreeState::Parent,
-                    _ => self.relationship
-                }
+            TreeState::Child => match self.relationship {
+                TreeState::Child => TreeState::Sibling,
+                TreeState::This => TreeState::Parent,
+                _ => self.relationship,
             },
-            TreeState::Sibling => {
-                match self.relationship {
-                    TreeState::This => TreeState::Sibling,
-                    _ => self.relationship
-                }
+            TreeState::Sibling => match self.relationship {
+                TreeState::This => TreeState::Sibling,
+                _ => self.relationship,
             },
-            _ => self.relationship
+            _ => self.relationship,
         };
 
         if target.hostname == self.hostname {
@@ -96,8 +92,10 @@ impl Worm {
     /// and the rest should simply be sent.
     pub fn new(max_segments: usize, hosts: Vec<String>) -> Worm {
         let mut buf = vec![0; 50];
-        let hostname = gethostname(&mut buf).expect("Error getting hostname")
-            .to_str().expect("Error using hostname as str");
+        let hostname = gethostname(&mut buf)
+            .expect("Error getting hostname")
+            .to_str()
+            .expect("Error using hostname as str");
 
         Worm {
             initial_hostname: String::from(hostname),
@@ -105,8 +103,8 @@ impl Worm {
             max_num_segments: max_segments,
             cur_num_segments: 1,
             observation_data: HashMap::new(),
-            current_segments: vec!(WormSegment::new(TreeState::This, hostname)),
-            hosts_to_ovserve: hosts
+            current_segments: vec![WormSegment::new(TreeState::This, hostname)],
+            hosts_to_ovserve: hosts,
         }
     }
 
@@ -114,11 +112,13 @@ impl Worm {
     pub fn get_data(&mut self) {
         let map: HashMap<String, String> = reqwest::get("http://localhost:8000/observation_data")
             .expect("Error requesting observation data")
-            .json().expect("Error parsing JSON");
+            .json()
+            .expect("Error parsing JSON");
 
         for (k, v) in &map {
             // Strip away port number from data
-            self.observation_data.insert(k[..k.len()-5].to_string(), v.to_string());
+            self.observation_data
+                .insert(k[..k.len() - 5].to_string(), v.to_string());
         }
     }
 
@@ -133,8 +133,12 @@ impl Worm {
         // Set timeout to 60 seconds
         let timeout = Duration::from_secs(60);
         let now = Instant::now();
-        let listener = TcpListener::bind(format!("{}:{}", &self.current_hostname , get_listen_port())).expect("Error binding to port");
-        listener.set_nonblocking(true).expect("Unable to make listener nonblocking");
+        let listener =
+            TcpListener::bind(format!("{}:{}", &self.current_hostname, get_listen_port()))
+                .expect("Error binding to port");
+        listener
+            .set_nonblocking(true)
+            .expect("Unable to make listener nonblocking");
         for conn in listener.incoming() {
             match conn {
                 Ok(stream) => {
@@ -147,26 +151,31 @@ impl Worm {
                             Message::NewSegment(segment) => {
                                 if !self.current_segments.contains(&segment) {
                                     self.current_segments.push(segment);
+                                    self.cur_num_segments += 1;
                                 }
-                            },
+                            }
                             Message::WantData(hostname) => {
-                                let _res = serde_json::to_writer(&stream, &self.observation_data.get(&hostname));
-                            },
+                                let _res = serde_json::to_writer(
+                                    &stream,
+                                    &self.observation_data.get(&hostname),
+                                );
+                            }
                             Message::SuicideNote(segment) => {
-                                if let Some(index) = self.current_segments.iter().position(|s| s == &segment) {
+                                if let Some(index) =
+                                    self.current_segments.iter().position(|s| s == &segment)
+                                {
                                     self.current_segments.remove(index);
                                     self.cur_num_segments -= 1;
                                 }
                             }
                         }
-
                     }
-                },
+                }
                 Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
                     if now.elapsed() > timeout {
                         break;
                     }
-                },
+                }
                 Err(e) => {
                     println!("Unable to accept connection: {:?}", e);
                 }
@@ -176,14 +185,26 @@ impl Worm {
 
     /// Send suicide note
     pub fn send_suicide_note(&self) {
-        for host in self.current_segments.iter().take(3) {
+        for host in self.current_segments.iter().take(5) {
             if host.relationship == TreeState::This {
                 continue;
             }
 
             let timeout = Duration::from_secs(1);
-            let stream = TcpStream::connect_timeout(&format!("{}:{}", host.hostname, self.calculate_port(&host.hostname.as_bytes())).parse().expect("Error parsing socket address"), timeout).expect("Could not connect to host");
-            let _res = serde_json::to_writer(&stream, &Message::SuicideNote(WormSegment::new(TreeState::This, &self.current_hostname)));
+            let hostname = &host.hostname;
+            if let Ok(addr) =
+                format!("{}:{}", hostname, self.calculate_port(hostname.as_bytes())).parse()
+            {
+                if let Ok(stream) = TcpStream::connect_timeout(&addr, timeout) {
+                    let _res = serde_json::to_writer(
+                        &stream,
+                        &Message::SuicideNote(WormSegment::new(
+                            TreeState::This,
+                            &self.current_hostname,
+                        )),
+                    );
+                }
+            }
         }
     }
 
@@ -196,9 +217,11 @@ impl Worm {
 
         // Read binary file into buffer and post it to wormgate
         let _n = f.read_to_end(&mut buf).expect("Could not read file to end");
-        let res = client.post(&format!("http://{}:8000/worm_entrance", host))
+        let res = client
+            .post(&format!("http://{}:8000/worm_entrance", host))
             .body(buf)
-            .send().expect("Error sending message");
+            .send()
+            .expect("Error sending message");
 
         println!("Post result: {:?}", res);
     }
@@ -220,10 +243,12 @@ impl Worm {
 
         // Update Worm state before sending it
         self.cur_num_segments += 1;
-        self.current_segments.push(WormSegment::new(TreeState::Child, host));
+        self.current_segments
+            .push(WormSegment::new(TreeState::Child, host));
 
         println!("Sending data to: {}:{}", host, port);
-        let stream = TcpStream::connect(&format!("{}:{}", host, port)).expect("Could not bind to socket");
+        let stream =
+            TcpStream::connect(&format!("{}:{}", host, port)).expect("Could not bind to socket");
         let _res = serde_json::to_writer(stream, &self);
     }
 
@@ -248,10 +273,18 @@ impl Worm {
             self.send_to_host(&host);
 
             // Gossip about it to some other host - with a timeout
-            for gossip_host in self.current_segments.iter().take(3) {
+            for gossip_host in self.current_segments.iter().take(5) {
                 let msg = Message::NewSegment(WormSegment::new(TreeState::Child, &host));
                 let timeout = Duration::from_secs(1);
-                let stream = TcpStream::connect_timeout(&format!("{}:{}", &gossip_host.hostname, self.calculate_port(gossip_host.hostname.as_bytes())).parse().expect("Error parsing address"), timeout).expect("Unable to connect to host");
+                let stream = TcpStream::connect_timeout(
+                    &format!(
+                        "{}:{}",
+                        &gossip_host.hostname,
+                        self.calculate_port(gossip_host.hostname.as_bytes())
+                    ).parse()
+                        .expect("Error parsing address"),
+                    timeout,
+                ).expect("Unable to connect to host");
                 let _res = serde_json::to_writer(&stream, &msg);
             }
         } else {
@@ -262,15 +295,19 @@ impl Worm {
     /// Return data to wormgate on current host
     pub fn return_data(&self) {
         let client = reqwest::Client::new();
-        let res = client.post("http://localhost:8000/observation_data")
+        let res = client
+            .post("http://localhost:8000/observation_data")
             .json(&self.observation_data)
-            .send().expect("Error uploading data");
+            .send()
+            .expect("Error uploading data");
         println!("Uploaded data to wormgate: {:?}", res);
     }
 
     /// Determine if we have all data we should have before returning it
     pub fn is_finished(&self) -> bool {
-        self.hosts_to_ovserve.iter().all(|ref host| self.observation_data.contains_key(host.as_str()))
+        self.hosts_to_ovserve
+            .iter()
+            .all(|ref host| self.observation_data.contains_key(host.as_str()))
     }
 }
 
@@ -281,8 +318,11 @@ fn daemonize() {
 
     match fork() {
         Ok(ForkResult::Parent { child, .. }) => {
-            println!("Continuing execution in parent process, the new child has pid: {}", child);
-        },
+            println!(
+                "Continuing execution in parent process, the new child has pid: {}",
+                child
+            );
+        }
         Ok(ForkResult::Child) => {
             println!("I am the child process!");
             if let Ok(pid) = setsid() {
@@ -291,14 +331,15 @@ fn daemonize() {
                 println!("Setsid failed :(");
             }
 
-            let c_self = CString::new(this.to_str().expect("No string?")).expect("Error creating C string");
+            let c_self =
+                CString::new(this.to_str().expect("No string?")).expect("Error creating C string");
             let c_arg = CString::new("-l").expect("Error creating C string");
             let c_new_name = CString::new("ls").expect("Error creating C string");
             match execv(&c_self, &[c_new_name, c_arg]) {
                 Ok(_) => println!("Everything is a-okay: I completed execv!"),
                 Err(e) => println!("Error executing execv: {}", e),
             }
-        },
+        }
         Err(_) => println!("Fork failed :("),
     }
 }
@@ -328,52 +369,58 @@ fn get_send_port(hostname: &[u8]) -> u64 {
 /// Listen for either the initial connection or a worm from parent segment
 /// Update worm segment status after receiving it from parent
 fn listen_for_worm() -> Result<Worm, &'static str> {
-        let mut buf = vec![0; 50];
-        let hostname = gethostname(&mut buf).expect("Error getting hostname")
-            .to_str().expect("Error using hostname as str");
-        println!("Listening at {}:{}", hostname, get_listen_port());
+    let mut buf = vec![0; 50];
+    let hostname = gethostname(&mut buf)
+        .expect("Error getting hostname")
+        .to_str()
+        .expect("Error using hostname as str");
+    println!("Listening at {}:{}", hostname, get_listen_port());
 
-        let listener = TcpListener::bind(format!("{}:{}", hostname , get_listen_port())).expect("Error binding to port");
+    let listener = TcpListener::bind(format!("{}:{}", hostname, get_listen_port()))
+        .expect("Error binding to port");
 
-        /* Accept TCP connection */
-        if let Ok((stream, addr)) = listener.accept() {
-            println!("Got some data from {:?}", addr);
-            /* Read shits from TCP stream */
-            if let Ok(worm) = serde_json::from_reader(stream) {
-                let mut worm: Worm = worm;
-                println!("Deserialized worm data from stream");
+    /* Accept TCP connection */
+    if let Ok((stream, addr)) = listener.accept() {
+        println!("Got some data from {:?}", addr);
+        /* Read shits from TCP stream */
+        if let Ok(worm) = serde_json::from_reader(stream) {
+            let mut worm: Worm = worm;
+            println!("Deserialized worm data from stream");
 
-                // Update worm segment data by calling the method for converting segment status
-                worm.current_hostname = hostname.to_string();
-                worm.current_segments = worm.current_segments.iter()
-                    .map(|segment| segment.send_to(&WormSegment::new(TreeState::Child, hostname)))
-                    .collect();
+            // Update worm segment data by calling the method for converting segment status
+            worm.current_hostname = hostname.to_string();
+            worm.current_segments = worm.current_segments
+                .iter()
+                .map(|segment| segment.send_to(&WormSegment::new(TreeState::Child, hostname)))
+                .collect();
 
-                Ok(worm)
-            /* No worm, but a start command */
-            } else {
-                println!("Unable to deserialize worm data - must be initial segment");
-                let file = File::open("hosts").expect("Unable to open hosts file");
-                let mut reader = BufReader::new(file);
-
-                /* Parse hostnames and create worm */
-                let mut hostnames: Vec<String> = vec![String::from(hostname)];
-                for line in reader.lines() {
-                    if let Ok(line) = line {
-                        hostnames.push(line);
-                    }
-                }
-                Ok(Worm::new(hostnames.len(), hostnames))
-            }
+            Ok(worm)
+        /* No worm, but a start command */
         } else {
-            Err("Could not read from TCP stream")
-        }
+            println!("Unable to deserialize worm data - must be initial segment");
+            let file = File::open("hosts").expect("Unable to open hosts file");
+            let mut reader = BufReader::new(file);
 
+            /* Parse hostnames and create worm */
+            let mut hostnames: Vec<String> = vec![String::from(hostname)];
+            for line in reader.lines() {
+                if let Ok(line) = line {
+                    hostnames.push(line);
+                }
+            }
+            Ok(Worm::new(hostnames.len(), hostnames))
+        }
+    } else {
+        Err("Could not read from TCP stream")
+    }
 }
 
 fn main() {
     if is_daemonized() {
-        println!("\nThis is now daemonized and was started with args: {:?}", env::args());
+        println!(
+            "\nThis is now daemonized and was started with args: {:?}",
+            env::args()
+        );
 
         /* Listen for worm or initial message */
         println!("Listening for a worm!");
@@ -401,14 +448,14 @@ fn main() {
                     0 => {
                         println!("Infecting another random host and gossiping about it");
                         worm.send_to_random_host();
-                    },
+                    }
                     1 => {
                         println!("Listening for gossip from other hosts");
                         worm.listen_for_gossip();
-                    },
+                    }
                     _ => {
                         println!("A random number modulo 2 should never be anything but 0 or 1");
-                    },
+                    }
                 }
             }
 
